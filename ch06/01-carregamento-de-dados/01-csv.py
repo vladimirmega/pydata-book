69359ed66203345e27180c4ef591650f44c710ae
vladimirmega <vladimir.gomes@megamanute.com.br>
2024-11-19 20:57:04 -0300

    Adicionada aula 1 cap 6

--- ch06/01-carregamento-de-dados/01-csv.py
+++ ch06/01-carregamento-de-dados/01-csv.py
@@ -1,7 +1,34 @@
 #%%
-from utils.paths import get_data_path
 import pandas as pd
+BASE_PATH = "/home/vladimir/Documents/Estudos/pydata-book/data/"
 
 #%%
-df = pd.read_csv("../../data/ex1.csv")
+# Lendo csv com cabeçalho
+df = pd.read_csv(BASE_PATH + "ex1.csv")
 df
+
+#%%
+# Lendo csv sem cabeçalho
+df2 = pd.read_csv(BASE_PATH + "ex2.csv",
+                  names=["a", "b", "c", "d", "message"])
+df2
+
+#%%
+# Lendo csv formando um índice hierárquico
+parsed = pd.read_csv(BASE_PATH + "csv_mindex.csv",
+                     index_col=["key1", "key2"])
+parsed
+
+#%%
+# Lendo csv com dados separados por espaços em branco
+result = pd.read_csv(BASE_PATH + "ex3.txt",
+                     sep="\s+")
+result
+
+#%%
+# Lendo csv pulando linhas
+result2 = pd.read_csv(BASE_PATH + "ex4.csv",
+                      skiprows=[0, 2, 3])
+result2
+
+#%%
